91d4c8a2b20622f0a0d21a90afa252d279bffa4b
Hanser0 <52728322+Hanser0@users.noreply.github.com>
2019-12-26 23:37:05 +0800

    提交

---
+++ Document/BuyChicken/BuyChicken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace baiqianmaibaiji
+{
+    class MainClass 
+    {
+        public static void Main(string[] args)
+        {
+            //公鸡的上线
+            for (int x = 0; x < 20; x++)
+            {
+                //母鸡的上线
+                for (int y = 0; y < 33; y++)
+                {
+                    //剩余小鸡
+                    int z = 100 - x - y;
+                    if ((z % 3 == 0) && (x * 5 + y * 3 + z / 3 == 100))
+                    {
+                        System.Console.WriteLine("公鸡:{0}只，母鸡:{1}只,小鸡:{2}只", x, y, z);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Document/DataStructuresAndAlgorithm/Hello World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithm
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!你好世界！");
+            Console.ReadLine();
+        }
+        
+    }
+}
